0c5563425ad489f119f17700bdfdfae19cc23300
Filip Popovski <filippopovski1998@gmail.com>
2023-10-06 12:35:49 +0200

    initial backend setup

---
+++ backend/jest.config.js
@@ -0,0 +1,7 @@
+export const transform = {
+    '^.+\\.ts?$': 'ts-jest',
+};
+export const testEnvironment = "node";
+export const testRegex = './src/.*\\.(test|spec)?\\.(js|ts)$';
+export const moduleFileExtensions = ['ts', 'js', 'json'];
+export const roots = ['<rootDir>/src'];
